c26eb588cbe3fd812b8700eeb6ab688a394d64c7
mart1ver <mart1ver@gmail.com>
2014-06-03 00:24:29 +0200

    ajout de la base mysql

---
+++ mysql/oressource.sql
@@ -0,0 +1,385 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.10.1deb1
+-- http://www.phpmyadmin.net
+--
+-- Client: localhost
+-- Généré le : Mar 03 Juin 2014 à 00:23
+-- Version du serveur: 5.5.34
+-- Version de PHP: 5.3.10-1ubuntu3.9
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données: `oressource`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `adherents`
+--
+
+CREATE TABLE IF NOT EXISTS `adherents` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `nom` text NOT NULL,
+  `prenom` text NOT NULL,
+  `adresse` text NOT NULL,
+  `mail` text NOT NULL,
+  `telephone` text NOT NULL,
+  `localisation` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `collectes`
+--
+
+CREATE TABLE IF NOT EXISTS `collectes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `id_type_collecte` int(11) NOT NULL,
+  `adherent` int(11) NOT NULL,
+  `localisation` int(11) NOT NULL,
+  `id_point_collecte` int(11) NOT NULL,
+  `commentaire` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `description_structure`
+--
+
+CREATE TABLE IF NOT EXISTS `description_structure` (
+  `nom` text NOT NULL,
+  `adresse` text NOT NULL,
+  `siret` int(11) NOT NULL,
+  `telephone` int(11) NOT NULL,
+  `mail` text NOT NULL,
+  `id_localité` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `filieres_sortie`
+--
+
+CREATE TABLE IF NOT EXISTS `filieres_sortie` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `nom` text NOT NULL,
+  `id_type_dechet` text NOT NULL,
+  `couleur` text NOT NULL,
+  `commentaire` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `localites`
+--
+
+CREATE TABLE IF NOT EXISTS `localites` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `nom` text NOT NULL,
+  `couleur` text NOT NULL,
+  `relation_openstreetmap` text NOT NULL,
+  `commentaire` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `objets_en_pret`
+--
+
+CREATE TABLE IF NOT EXISTS `objets_en_pret` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timstamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `description` text NOT NULL,
+  `ressource` text NOT NULL,
+  `numero_facture` text NOT NULL,
+  `valeur_remboursement` text NOT NULL,
+  `numero_serie` text NOT NULL,
+  `photo` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `pesees_collectes`
+--
+
+CREATE TABLE IF NOT EXISTS `pesees_collectes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `masse` int(11) NOT NULL,
+  `id_collecte` int(11) NOT NULL,
+  `id_type_dechet` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `pesees_sorties`
+--
+
+CREATE TABLE IF NOT EXISTS `pesees_sorties` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `masse` int(11) NOT NULL,
+  `id_sortie` int(11) NOT NULL,
+  `id_type_dechet` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `points_collecte`
+--
+
+CREATE TABLE IF NOT EXISTS `points_collecte` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `nom` text NOT NULL,
+  `adresse` text NOT NULL,
+  `couleur` text NOT NULL,
+  `commentaire` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Contenu de la table `points_collecte`
+--
+
+INSERT INTO `points_collecte` (`id`, `timestamp`, `nom`, `adresse`, `couleur`, `commentaire`) VALUES
+(1, '2014-04-08 18:06:36', 'Point d''apport volontaire en boutique', '125 rue du chemin vert', '', 'le point par lequel tout passe ...'),
+(2, '2014-06-02 19:52:56', 'deuxieme point', '45 rue du du', '', 'oui mais non');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `points_sortie`
+--
+
+CREATE TABLE IF NOT EXISTS `points_sortie` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `nom` text NOT NULL,
+  `adresse` text NOT NULL,
+  `couleur` text NOT NULL,
+  `commentaire` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Contenu de la table `points_sortie`
+--
+
+INSERT INTO `points_sortie` (`id`, `timestamp`, `nom`, `adresse`, `couleur`, `commentaire`) VALUES
+(1, '2014-04-08 18:19:28', 'sorties hors boutique', '125 rue du chemin vert', '', 'point de sortie hors boutique principale de la ressourcerie de la petite rockette');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `points_vente`
+--
+
+CREATE TABLE IF NOT EXISTS `points_vente` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `nom` text NOT NULL,
+  `adresse` text NOT NULL,
+  `couleur` text NOT NULL,
+  `commentaire` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Contenu de la table `points_vente`
+--
+
+INSERT INTO `points_vente` (`id`, `timestamp`, `nom`, `adresse`, `couleur`, `commentaire`) VALUES
+(1, '2014-04-08 18:17:25', 'caisse de la ressourcerie', '125 rue du chemin vert', '', 'la caisse principale de la ressourcerie de la petite rockette              ');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `prets`
+--
+
+CREATE TABLE IF NOT EXISTS `prets` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_objet_en_pret` text NOT NULL,
+  `date_pret` text NOT NULL,
+  `date_retour` text NOT NULL,
+  `id_adherent` text NOT NULL,
+  `nom` text NOT NULL,
+  `prenom` text NOT NULL,
+  `adresse` text NOT NULL,
+  `mail` text NOT NULL,
+  `date_depot_caution` text NOT NULL,
+  `date_rendu_caution` text NOT NULL,
+  `montant_caution` text NOT NULL,
+  `id_utilisateur` text NOT NULL,
+  `commentaire` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `recettes_points_sorties`
+--
+
+CREATE TABLE IF NOT EXISTS `recettes_points_sorties` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `id_filiere` int(11) NOT NULL,
+  `recette` int(11) NOT NULL,
+  `commentaire` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `sorties`
+--
+
+CREATE TABLE IF NOT EXISTS `sorties` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `id_filiere` int(11) NOT NULL,
+  `commentaire` text NOT NULL,
+  `id_point_sortie` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `type_collecte`
+--
+
+CREATE TABLE IF NOT EXISTS `type_collecte` (
+  `id` int(11) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `nom` text NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `type_dechets`
+--
+
+CREATE TABLE IF NOT EXISTS `type_dechets` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `nom` text NOT NULL,
+  `description` text NOT NULL,
+  `couleur` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Contenu de la table `type_dechets`
+--
+
+INSERT INTO `type_dechets` (`id`, `timestamp`, `nom`, `description`, `couleur`) VALUES
+(1, '2014-04-08 18:21:16', 'deee', 'déchets équipements électroniques et électroménagers ', ''),
+(2, '2014-04-08 18:21:40', 'mobilier', 'mobilier', ''),
+(3, '2014-04-08 18:22:10', 'textile', 'textile', '');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `type_objets`
+--
+
+CREATE TABLE IF NOT EXISTS `type_objets` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `nom` text NOT NULL,
+  `desciption` text NOT NULL,
+  `couleur` text NOT NULL,
+  `id_type_dechet` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateurs`
+--
+
+CREATE TABLE IF NOT EXISTS `utilisateurs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `niveau` text NOT NULL,
+  `nom` text NOT NULL,
+  `prenom` text NOT NULL,
+  `mail` text NOT NULL,
+  `pass` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Contenu de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id`, `timestamp`, `niveau`, `nom`, `prenom`, `mail`, `pass`) VALUES
+(2, '2014-04-08 18:02:48', 'abcesgh', 'admin', 'admin', 'admin@admin.com', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `vendus`
+--
+
+CREATE TABLE IF NOT EXISTS `vendus` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_vente` int(11) NOT NULL,
+  `id_type_objet` int(11) NOT NULL,
+  `prix` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ventes`
+--
+
+CREATE TABLE IF NOT EXISTS `ventes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `adherent` text NOT NULL,
+  `commentaire` text NOT NULL,
+  `id_point_vente` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
