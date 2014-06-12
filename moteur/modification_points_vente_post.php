b7809ddc9cc1507675572228f40706d44db39164
mart1ver <mart1ver@gmail.com>
2014-06-12 17:36:28 +0200

    peauffinage de la gestion des points de vente

---
+++ moteur/modification_points_vente_post.php
@@ -0,0 +1,41 @@
+<?php
+//martin vert
+// Connexion à la base de données
+try
+{
+include('dbconfig.php');
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+ 
+// Insertion du post à l'aide d'une requête préparée
+// mot de passe crypté md5 
+
+// Insertion du post à l'aide d'une requête préparée
+$req = $bdd->prepare('UPDATE points_vente SET nom = :nom, adresse = :adresse , commentaire = :commentaire, couleur = :couleur  WHERE id = :id');
+$req->execute(array('nom' => $_POST['nom'],'adresse' => $_POST['adresse'],'commentaire' => $_POST['commentaire'],'couleur' => $_POST['couleur'],'id' => $_POST['id']));
+
+  $req->closeCursor();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Redirection du visiteur vers la page de gestion des points de collecte
+header('Location:../ifaces/edition_points_vente.php');
+?>
