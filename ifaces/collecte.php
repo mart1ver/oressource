39cd109f28926f8e2ec62d3257c33b15c3f1dc6c
mart1ver <mart1ver@gmail.com>
2014-06-28 22:27:06 +0200

    un peut de javascript dans le formulaire de collecte

--- ifaces/collecte.php
+++ ifaces/collecte.php
@@ -1,5 +1,103 @@
 <?php session_start(); ?>
 <?php include "tete.php" ?>
+
+
+
+
+
+
+
+
+<script type="text/javascript">
+function number_write(x)
+{
+  var text_box = document.getElementById("number");
+  
+   
+ text_box.value = text_box.value + x;
+  
+}
+
+function number_clear()
+{
+  document.getElementById("number").value = "";
+}
+function tdechet_write(y)
+{
+  
+document.getElementById(y).innerText = parseInt(document.getElementById(y).innerText) + parseInt(document.getElementById("number").value)  ;
+
+ 
+  
+}
+function tdechet_clear()
+{
+
+
+
+<?php 
+          
+            try
+            {
+            // On se connecte à MySQL
+            include('../moteur/dbconfig.php');
+            }
+            catch(Exception $e)
+            {
+            // En cas d'erreur, on affiche un message et on arrête tout
+            die('Erreur : '.$e->getMessage());
+            }
+ 
+            // Si tout va bien, on peut continuer
+ 
+            // On recupère tout le contenu de la table point de collecte
+            $reponse = $bdd->query('SELECT * FROM type_dechets');
+ 
+           // On affiche chaque entree une à une
+           while ($donnees = $reponse->fetch())
+           {
+
+           ?>
+    
+            
+
+
+
+
+
+document.getElementById('<?php echo$donnees['nom']?>').innerText = "0"  ;
+
+
+
+
+
+
+   
+              <?php }
+
+              $reponse->closeCursor(); // Termine le traitement de la requête
+                ?>
+
+
+
+  
+
+
+ 
+  
+}
+</script>
+
+
+
+
+
+
+
+
+
+
+
  <p class="text-center"><h1>Point de collecte</h1> 
            </p> 
            <b>nom du point de collecte</b>
@@ -24,32 +122,62 @@
         
 bon d'apport:
 
+
+
+
+<?php 
+          
+            try
+            {
+            // On se connecte à MySQL
+            include('../moteur/dbconfig.php');
+            }
+            catch(Exception $e)
+            {
+            // En cas d'erreur, on affiche un message et on arrête tout
+            die('Erreur : '.$e->getMessage());
+            }
+ 
+            // Si tout va bien, on peut continuer
+ 
+            // On recupère tout le contenu de la table point de collecte
+            $reponse = $bdd->query('SELECT * FROM type_dechets');
+ 
+           // On affiche chaque entree une à une
+           while ($donnees = $reponse->fetch())
+           {
+
+           ?>
+    
+            
+
+
+
+
+
 <ul class="list-group">
   <li class="list-group-item">
-    <span class="badge">masse</span>
-    type
+   
+    <span class="badge" id="<?php echo$donnees['nom']?>">0</span>
+    <?php echo$donnees['nom']?>
+
   </li>
-  <li class="list-group-item">
-    <span class="badge">masse</span>
-    type
-  </li>
-  <li class="list-group-item">
-    <span class="badge">masse</span>
-    type
-  </li>
-  <li class="list-group-item">
-    <span class="badge">masse</span>
-    type
-  </li>
-  <li class="list-group-item">
-    <span class="badge">masse</span>
-    type
-  </li>
-  <li class="list-group-item">
-    <span class="badge">masse</span>
-    type
-  </li>
+
+
+
+
+
+
+   
+              <?php }
+
+              $reponse->closeCursor(); // Termine le traitement de la requête
+                ?>
+
 </ul>
+
+
+
 
         </div>  
           <div class="col-md-4" >
@@ -80,7 +208,7 @@ bon d'apport:
     
             <li>
               
-            <button class="btn btn-default btn-lg" data-value="4"><?php echo$donnees['nom']?></button> 
+            <button class="btn btn-default" onclick="tdechet_write('<?php echo$donnees['nom']?>');" ><?php echo$donnees['nom']?></button> 
               </a>
           </li>
           <br>
@@ -98,29 +226,43 @@ bon d'apport:
     	<div class="row">
     	<div class="input-group">
   
-  <input type="text" class="form-control" placeholder="Masse"><span class="input-group-addon">Kg.</span>
+  <input type="text" class="form-control" placeholder="Masse" id="number" name="num" ><span class="input-group-addon">Kg.</span>
 </div>
 </div><br>
         <div class="row">
-            <button class="btn btn-default btn-lg" data-value="1">1</button>
-            <button class="btn btn-default btn-lg" data-value="2">2</button>
-            <button class="btn btn-default btn-lg" data-value="3">3</button>
+            <button class="btn btn-default btn-lg" onclick="number_write('1');" data-value="1">1</button>
+            <button class="btn btn-default btn-lg" onclick="number_write('2');" data-value="2">2</button>
+            <button class="btn btn-default btn-lg" onclick="number_write('3');" data-value="3">3</button>
         </div>
         <div class="row">
-            <button class="btn btn-default btn-lg" data-value="4">4</button>
-            <button class="btn btn-default btn-lg" data-value="5">5</button>
-            <button class="btn btn-default btn-lg" data-value="6">6</button>
+            <button class="btn btn-default btn-lg" onclick="number_write('4');" data-value="4">4</button>
+            <button class="btn btn-default btn-lg" onclick="number_write('5');" data-value="5">5</button>
+            <button class="btn btn-default btn-lg" onclick="number_write('6');" data-value="6">6</button>
         </div>
         <div class="row">
-            <button class="btn btn-default btn-lg" data-value="1">7</button>
-            <button class="btn btn-default btn-lg" data-value="2">8</button>
-            <button class="btn btn-default btn-lg" data-value="3">9</button>
+            <button class="btn btn-default btn-lg" onclick="number_write('7');" data-value="7">7</button>
+            <button class="btn btn-default btn-lg" onclick="number_write('8');" data-value="8">8</button>
+            <button class="btn btn-default btn-lg" onclick="number_write('9');" data-value="9">9</button>
         </div>
         <div class="row">
-            <button class="btn btn-default btn-lg" data-value="C">C</button>
-            <button class="btn btn-default btn-lg" data-value="0">0</button>
-            <button class="btn btn-default btn-lg" data-value=",">,</button>
+            <button class="btn btn-default btn-lg" onclick="number_clear();" data-value="C">C</button>
+            <button class="btn btn-default btn-lg" onclick="number_write('0');" data-value="0">0</button>
+            <button class="btn btn-default btn-lg" onclick="number_write(',');" data-value=",">,</button>
         </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
         </div>
 
@@ -130,7 +272,7 @@ bon d'apport:
       <div class="row">
         <div class="col-md-4" >
         	<button class="btn btn-default btn-lg" data-value="C">c'est pesé!</button>
-            <button class="btn btn-default btn-lg" data-value="0">reset</button>
+            <button class="btn btn-default btn-lg" onclick="tdechet_clear();" data-value="0">reset</button>
             <button class="btn btn-default btn-lg" data-value=",">imprimer</button>
         <br>
             
@@ -142,8 +284,7 @@ bon d'apport:
         </div>
         <div class="col-md-4" >
           
-         <button class="btn btn-default btn-lg" data-value="C">remboursement</button>
-            <button class="btn btn-default btn-lg" data-value="0">total caisse</button>
+        
          
         </div>
       </div>
