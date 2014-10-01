f1a6a7e697c99284534201f33a0aacdcc08e5923
mart1ver <mart1ver@gmail.com>
2014-10-01 21:11:22 +0200

    mise en page sur collecte.php

--- ifaces/collecte.php
+++ ifaces/collecte.php
@@ -105,7 +105,7 @@ else // SINON (la variable ne contient ni Oui ni Non, on ne peut pas agir)
 <br>
      
        <legend>
-        <blockquote>
+        <h2>
         <?php 
             try
             {
@@ -127,80 +127,34 @@ else // SINON (la variable ne contient ni Oui ni Non, on ne peut pas agir)
             }
             $reponse->closeCursor(); // Termine le traitement de la requête
         ?>
-      </blockquote>
+      </h2>
        </legend>
        
       
 
 <div class="row">
   <div class="col-md-3 col-md-offset-1" >
-    <form action="../moteur/collecte_post.php" method="post">
-    <label for="id_type_collecte">Type de collecte:</label>  
-    <select name ="id_type_collecte" id ="id_type_collecte" class="form-control" autofocus >
-            <?php 
-            try
-            {
-            // On se connecte à MySQL
-            include('../moteur/dbconfig.php');
-            }
-            catch(Exception $e)
-            {
-            // En cas d'erreur, on affiche un message et on arrête tout
-            die('Erreur : '.$e->getMessage());
-            }
-            // On affiche une liste déroulante des types de collecte visibles
-            $reponse = $bdd->query('SELECT * FROM type_collecte WHERE visible = "oui"');
-            // On affiche chaque entrée une à une
-            while ($donnees = $reponse->fetch())
-            {
-            ?>
-      <option value = "<?php echo$donnees['id']?>" ><?php echo$donnees['nom']?></option>
-            <?php }
-            $reponse->closeCursor(); // Termine le traitement de la requête
-            ?>
-    </select>
+ 
   </div>
    
-<div class="col-md-3" >
-<label for="loc">Localité:</label>  
-    <select name ="loc" id ="loc" class="form-control">
-            <?php 
-            try
-            {
-            // On se connecte à MySQL
-            include('../moteur/dbconfig.php');
-            }
-            catch(Exception $e)
-            {
-            // En cas d'erreur, on affiche un message et on arrête tout
-            die('Erreur : '.$e->getMessage());
-            }
-            // On affiche une liste déroulante des localités visibles
-            $reponse = $bdd->query('SELECT * FROM localites WHERE visible = "oui"');
-            // On affiche chaque entrée une à une
-            while ($donnees = $reponse->fetch())
-            {
-            ?>
 
-      <option value = "<?php echo$donnees['id']?>" ><?php echo$donnees['nom']?></option>
-            <?php }
-            $reponse->closeCursor(); // Termine le traitement de la requête
-            ?>
-    </select>
-    
-</div>
 <br> 
- <div class="col-md-3 " >
-   <br>
-    <input type="hidden" name ="id_point_collecte" id="id_point_collecte" value="<?php echo $_GET['numero']?>">
-    <input name ="adh" id ="adh" type="checkbox" ><label for="adh">Adhére à l'association</label> <a href="adhesions.php"  target="_blank"><span style="float:right;" class="glyphicon glyphicon-pencil"></span></a>
-  </div> 
+
   </div>
 
 <div class="row">
 <br>
   <div class="col-md-3 col-md-offset-1" >
-  <label>Bon d'apport:</label>
+
+<div class="panel panel-info">
+        <div class="panel-heading">
+          <form action="../moteur/collecte_post.php" method="post">
+    <h3 class="panel-title"><label>Bon d'apport:</label></h3>
+  </div>
+  <div class="panel-body"> 
+
+
+  
             <?php 
             try
             {
@@ -234,14 +188,33 @@ else // SINON (la variable ne contient ni Oui ni Non, on ne peut pas agir)
     </ul>
     <input type="hidden" value="0" name ="najout" id="najout">
   <button class="btn btn-primary btn-lg">C'EST PESÉ!</button>
-</form>
+
 <button class="btn btn-primary btn-lg"  align="center"><span class="glyphicon glyphicon-print"></span></button>
         <button class="btn btn-warning btn-lg" onclick="tdechet_clear();"><span class="glyphicon glyphicon-refresh"></button>
 
   <br>
-  </div>  
-  <div class="col-md-2" >
-  <label>Types d'objets collectés:</label><br>
+
+
+</div> 
+
+
+
+    </div>
+     </div> 
+     <div class="col-md-2" >
+<div class="panel panel-info">
+        <div class="panel-heading">
+     <h3 class="panel-title"><label>Informations :</label></h3>
+  </div>
+  <div class="panel-body"> 
+
+
+
+
+    <label for="id_type_collecte">Type de collecte:</label>  
+    <select name ="id_type_collecte" id ="id_type_collecte" class="form-control" autofocus required>
+       <option value="" selected="selected"></option> 
+
             <?php 
             try
             {
@@ -253,29 +226,22 @@ else // SINON (la variable ne contient ni Oui ni Non, on ne peut pas agir)
             // En cas d'erreur, on affiche un message et on arrête tout
             die('Erreur : '.$e->getMessage());
             }
- 
-            // Si tout va bien, on peut continuer
- 
-            // ON AFFICHE un bouton par type de déchet visible(id impair)
-            $reponse = $bdd->query('SELECT * FROM type_dechets WHERE visible = "oui" AND MOD(id,2)=1');
- 
-           // On affiche chaque entrée une à une
-           while ($donnees = $reponse->fetch())
-           {
+            // On affiche une liste déroulante des types de collecte visibles
+            $reponse = $bdd->query('SELECT * FROM type_collecte WHERE visible = "oui"');
+            // On affiche chaque entrée une à une
+            while ($donnees = $reponse->fetch())
+            {
+            ?>
+      <option value = "<?php echo$donnees['id']?>" ><?php echo$donnees['nom']?></option>
+            <?php }
+            $reponse->closeCursor(); // Termine le traitement de la requête
+            ?>
+    </select>
 
-           ?>
-  <button class="btn btn-default btn-sm" onclick="tdechet_write('<?php echo$donnees['nom']?>','<?php echo$donnees['id']?>');" ><span class="badge" id="cool" style="background-color:<?php echo$donnees['couleur']?>"><?php echo$donnees['nom']?></span>
- </button> 
-  
-  <br>
-  <br>
-           <?php }
-           $reponse->closeCursor(); // Termine le traitement de la requête
-           ?>
-  </div>
-  <div class="col-md-2" >
-  <br><br>
-            <?php  
+<label for="loc">Localité:</label>  
+    <select name ="loc" id ="loc" class="form-control" required>
+       <option value="" selected="selected"></option>
+            <?php 
             try
             {
             // On se connecte à MySQL
@@ -286,21 +252,77 @@ else // SINON (la variable ne contient ni Oui ni Non, on ne peut pas agir)
             // En cas d'erreur, on affiche un message et on arrête tout
             die('Erreur : '.$e->getMessage());
             }
-            // ON AFFICHE un bouton par type de déchet visible(id pair)
-            $reponse = $bdd->query('SELECT * FROM type_dechets WHERE visible = "oui" AND MOD(id,2)=0');
+            // On affiche une liste déroulante des localités visibles
+            $reponse = $bdd->query('SELECT * FROM localites WHERE visible = "oui"');
             // On affiche chaque entrée une à une
             while ($donnees = $reponse->fetch())
             {
             ?>
-  <button class="btn btn-default btn-sm" onclick="tdechet_write('<?php echo$donnees['nom']?>','<?php echo$donnees['id']?>');" ><span class="badge" id="cool" style="background-color:<?php echo$donnees['couleur']?>"><?php echo$donnees['nom']?></span>
- </button> 
-  <br><br>
-   
+
+      <option value = "<?php echo$donnees['id']?>" ><?php echo$donnees['nom']?></option>
             <?php }
             $reponse->closeCursor(); // Termine le traitement de la requête
             ?>
+    </select>
 
+<br>
+
+<input type="hidden" name ="id_point_collecte" id="id_point_collecte" value="<?php echo $_GET['numero']?>">
+    <input name ="adh" id ="adh" type="checkbox" ><label for="adh">Adhére à l'association</label> <a href="adhesions.php"  target="_blank"><span style="float:right;" class="glyphicon glyphicon-pencil"></span></a>
+
+</div>
+</div>
+
+
+</form>
   </div>
+
+  <div class="col-md-3" >
+
+<div class="panel panel-info">
+        <div class="panel-heading">
+    <h3 class="panel-title"><label>Type d'objet:</label></h3>
+  </div>
+  <div class="panel-body"> 
+      
+
+
+            <?php 
+            try
+            {
+            // On se connecte à MySQL
+            include('../moteur/dbconfig.php');
+            }
+            catch(Exception $e)
+            {
+            // En cas d'erreur, on affiche un message et on arrête tout
+            die('Erreur : '.$e->getMessage());
+            }
+            // On recupère tout le contenu de la table point de collecte
+            $reponse = $bdd->query('SELECT * FROM type_dechets WHERE visible = "oui"');
+ 
+           // On affiche chaque entree une à une
+           while ($donnees = $reponse->fetch())
+           {
+           ?>
+      <div class="btn-group">
+      <button class="btn btn-default" style="margin-left:8px; margin-top:16px;" onclick="tdechet_write('<?php echo$donnees['nom']?>','<?php echo$donnees['id']?>');" ><span class="badge" id="cool" style="background-color:<?php echo$donnees['couleur']?>"><?php echo$donnees['nom']?></span>
+ </button>
+      
+    </div>
+   
+                <?php }
+                $reponse->closeCursor(); // Termine le traitement de la requête
+                ?>
+    </div> 
+
+
+
+    </div>
+    <br><br><br>
+     
+    </div>
+
   
   <div class="col-md-3" style="width: 220px;" >
 
