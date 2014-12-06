0d9aed0eadd15761f2af2d56e3dba4967e4a3724
mart1ver <mart1ver@gmail.com>
2014-12-07 00:45:41 +0100

    bafouillage sur les bilans collecte

--- ifaces/bilanc.php
+++ ifaces/bilanc.php
@@ -216,6 +216,12 @@ $time_fin = $time_fin." 23:59:59";
 // on determine la masse totale collècté sur cete periode
 
 
+           //on obtien la couleur de la localité dans la base
+
+
+
+
+
             try
             {
             // On se connecte à MySQL
@@ -226,19 +232,20 @@ $time_fin = $time_fin." 23:59:59";
             // En cas d'erreur, on affiche un message et on arrête tout
             die('Erreur : '.$e->getMessage());
             }
- 
             // Si tout va bien, on peut continuer
-            /*
-SELECT SUM(masse),timestamp FROM pesees_collectes WHERE  `timestamp`BETWEEN '2014-09-18 00:00:00' AND '2014-09-24 23:59:59'
-            */
- $req = $bdd->prepare("SELECT SUM(pesees_collectes.masse),pesees_collectes.timestamp  FROM pesees_collectes  WHERE  `pesees_collectes.timestamp` BETWEEN :du AND :au  ");
+            // On recupère tout le contenu de la table point de vente
+
+
+$req = $bdd->prepare("SELECT SUM(pesees_collectes.masse)AS total   FROM pesees_collectes  WHERE  DATE(pesees_collectes.timestamp) BETWEEN DATE(:du) AND DATE(:au)  ");
 $req->execute(array('du' => $time_debut,'au' => $time_fin ));
 $donnees = $req->fetch();
-     
-echo $donnees['SUM(pesees_collectes.masse)'].' Kgs.';
+$mtotcolo = $donnees['total'];
+echo $donnees['total']."Kgs.";
+            
+              $reponse->closeCursor(); // Termine le traitement de la requête
+               
 
-$req->closeCursor(); // Termine le traitement de la requête
-
+if ($_GET['numero'] == 0) {
 
 
   ?>
@@ -261,7 +268,7 @@ $req->closeCursor(); // Termine le traitement de la requête
             /*
 
             */
- $req = $bdd->prepare("SELECT COUNT(id) FROM points_collecte WHERE  `timestamp` < :au ");//SELECT `titre_affectation` FROM affectations WHERE titre_affectation = "conssomables" LIMIT 1
+ $req = $bdd->prepare("SELECT COUNT(id) FROM points_collecte");//SELECT `titre_affectation` FROM affectations WHERE titre_affectation = "conssomables" LIMIT 1
 $req->execute(array('au' => $time_fin ));
 $donnees = $req->fetch();
      
@@ -271,7 +278,10 @@ $req->closeCursor(); // Termine le traitement de la requête
 
 
 
-  ?> Point(s) de collecte.</h3>
+  ?> Point(s) de collecte.
+
+<?php } ?>
+</h3>
   </div>
   <div class="panel-body">
     
@@ -285,15 +295,74 @@ $req->closeCursor(); // Termine le traitement de la requête
         </tr>
     </thead>
     <tbody>
-        <tr data-toggle="collapse" data-target=".parmasse1" class="active">
-            <td>a dom</td>
-            <td>125</td>
-            <td>58</td>
+       
+        <?php
+// on determine la masse totale collècté sur cete periode
+
+
+     
+
+
+
+
+  ?>
+      
+        
+        <?php 
+            try
+            {
+            // On se connecte à MySQL
+            include('../moteur/dbconfig.php');
+            }
+            catch(Exception $e)
+            {
+            // En cas d'erreur, on affiche un message et on arrête tout
+            die('Erreur : '.$e->getMessage());
+            }
+ 
+            // Si tout va bien, on peut continuer
+ 
+            // On recupère tout le contenu de la table affectations
+
+
+
+
+
+
+
+            $reponse = $bdd->prepare('SELECT 
+type_collecte.nom,SUM(`pesees_collectes`.`masse`) somme,pesees_collectes.timestamp,type_collecte.id
+
+
+FROM 
+pesees_collectes,collectes,type_collecte
+
+WHERE
+  pesees_collectes.timestamp BETWEEN :du AND :au AND
+type_collecte.id =  collectes.id_type_collecte AND pesees_collectes.id_collecte = collectes.id
+
+ 
+GROUP BY id_type_collecte');
+
+
+ $reponse->execute(array('du' => $time_debut,'au' => $time_fin ));
+           // On affiche chaque entree une à une
+           while ($donnees = $reponse->fetch())
+           {
+
+           
+            ?>
+      
+            <tr data-toggle="collapse" data-target=".parmasse<?php echo $donnees['id']?>" class="active">
+            <td><?php echo $donnees['nom'] ?></td>
+            <td><?php echo $donnees['somme'] ?></td>
+            <td><?php echo  round($donnees['somme']*100/$mtotcolo, 2)   ; ?></td>
             
         </tr>
       
-        
-        <tr class="collapse parmasse1">
+     
+
+ <tr class="collapse parmasse<?php echo $donnees['id']?>">
             <td class="hiddenRow">
                deee
             </td >
@@ -307,52 +376,15 @@ $req->closeCursor(); // Termine le traitement de la requête
         </tr>
 
 
-        <tr class="collapse parmasse1">
-            <td class="hiddenRow">
-                textile
-            </td >
-            <td class="hiddenRow">
-                25kgs
-            </td>
-            <td class="hiddenRow">
-                25%
-            </td>
-            
-        </tr>
- <tr data-toggle="collapse" data-target=".parmasse2" class="active">
-            <td>apport vol.</td>
-            <td>125</td>
-            <td>58</td>
-            
-        </tr>
-      
         
-        <tr class="collapse parmasse2">
-            <td class="hiddenRow">
-               mobilier
-            </td>
-            <td class="hiddenRow">
-                100kgs
-            </td>
-            <td class="hiddenRow">
-                75%
-            </td>
-            
-        </tr>
 
 
-        <tr class="collapse parmasse2">
-            <td class="hiddenRow">
-                autres
-            </td>
-            <td class="hiddenRow">
-                25kgs
-            </td>
-            <td class="hiddenRow">
-                25%
-            </td>
-            
-        </tr>
+
+      <?php
+
+             }
+              $reponse->closeCursor(); // Termine le traitement de la requête
+                ?>
 
       
     </tbody>
@@ -405,7 +437,13 @@ $req->closeCursor(); // Termine le traitement de la requête
 
 
 
+if ($_GET['numero'] == 0) {
   ?>
+
+
+
+
+
   , sur <?php
 // on determine le nombre de points de collecte
 
@@ -435,7 +473,12 @@ $req->closeCursor(); // Termine le traitement de la requête
 
 
 
-  ?> Point(s) de collecte.</h3>
+  ?> Point(s) de collecte.
+
+<?php } ?>
+
+
+</h3>
   </div>
   <div class="panel-body">
     
