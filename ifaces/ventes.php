c91df03d434134b796b414061560d15c028b2fdc
damien clochard <damien@dalibo.info>
2015-03-08 16:34:21 +0100

    FIX : erreur de vérification des droits utilisateurs sur la page vente

--- ifaces/ventes.php
+++ ifaces/ventes.php
@@ -1,6 +1,6 @@
 <?php session_start(); 
 //Vérification des autorisations de l'utilisateur et des variables de session requises pour l'affichage de cette page:
-if (isset($_SESSION['id']) AND $_SESSION['systeme'] = "oressource" AND (strpos($_SESSION['niveau'], 'v'.$_GET['numero']) !== false))
+if (isset($_SESSION['id']) AND $_SESSION['systeme'] = "oressource" AND (strpos($_SESSION['niveau'], 'v') !== false))
       {include "tete.php";?>
 
 
