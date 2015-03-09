9429c589f278c7eac2fbefe376e6819cd4b71c0e
mart1ver <mart1ver@gmail.com>
2015-03-09 22:16:04 +0100

    bilanv_alter

--- ifaces/bilanv_alter.php
+++ ifaces/bilanv_alter.php
@@ -330,7 +330,7 @@ else // si numero ==! 0*********************************************************
 {
 ?>
   <div class="row">
-  <div class="col-md-1">.col-md-1
+  <div class="col-md-6">
   <?php
 echo "-chiffre total dégagé : (remboursemments déduits) ";
 try
