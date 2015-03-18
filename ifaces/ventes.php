5bc6b48bdd1860b80a68b984ebd1fb19032970db
Martin VERT <mart1ver@gmail.com>
2015-03-18 16:23:39 +0100

    switch lot/unitéà la caisse

--- ifaces/ventes.php
+++ ifaces/ventes.php
@@ -123,12 +123,12 @@ else // SINON (la variable ne contient ni Oui ni Non, on ne peut pas agir)
   </div>
   <div class="panel-body"> 
      
-
-<input type="checkbox" name="my-checkbox"  align="right" checked  data-on-text="unité" data-off-text="lot" data-handle-width="45" data-size="small">
+<p align="right">
+<input type="checkbox" name="my-checkbox"   checked  data-on-text="unité" data-off-text="lot" data-handle-width="45" data-size="small">
 <script type="text/javascript">
 $("[name='my-checkbox']").bootstrapSwitch();
 </script>
-
+<p>
 
 <br>
 
