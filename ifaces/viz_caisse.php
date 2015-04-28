a6908770c18db488dc87372aa1c78854694efb67
Martin VERT <mart1ver@gmail.com>
2015-04-28 16:46:26 +0200

    config

--- ifaces/viz_caisse.php
+++ ifaces/viz_caisse.php
@@ -99,10 +99,10 @@ GROUP BY nom'
 $req = $bdd->prepare('SELECT ventes.id,ventes.timestamp ,moyens_paiement.nom moyen, moyens_paiement.couleur coul, ventes.commentaire ,ventes.last_hero_timestamp lht 
                        FROM ventes ,moyens_paiement 
                        WHERE ventes.id_point_vente = :id_point_vente 
-                       AND ventes.id_moyen_paiement = moyens_paiement.id AND DATE(ventes.timestamp) =DATE(CURRENT_TIMESTAMP()) LIMIT 0,:nb_viz_caisse' );
+                       AND ventes.id_moyen_paiement = moyens_paiement.id AND DATE(ventes.timestamp) =DATE(CURRENT_TIMESTAMP()) LIMIT 0,:offset' );
 
 $req->bindValue('id_point_vente' , $_GET['numero'] , PDO::PARAM_INT );
-$req->bindValue('nb_viz_caisse' , $_SESSION['nb_viz_caisse'] , PDO::PARAM_INT );
+$req->bindValue('offset' , $_SESSION['nb_viz_caisse'] , PDO::PARAM_INT );
 $req->execute();
 
 
