6fc97ca66ba5b399bf2ab5dfd2c97923203dad92
Martin VERT <mart1ver@gmail.com>
2015-04-28 17:38:57 +0200

    config

--- ifaces/viz_vente.php
+++ ifaces/viz_vente.php
@@ -52,8 +52,7 @@ else // SINON (la variable ne contient ni Oui ni Non, on ne peut pas agir)
             <th>Prix</th>
             <th>Auteur de la ligne</th>
             
-            <th>Modifié par</th>
-            <th>Le:</th>
+            
 
             
           </tr>
