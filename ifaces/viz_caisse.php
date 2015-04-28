36944f7d7ad65f0dd57c4f7f265dab9d22acc99a
Martin VERT <mart1ver@gmail.com>
2015-04-28 17:00:26 +0200

    config

--- ifaces/viz_caisse.php
+++ ifaces/viz_caisse.php
@@ -101,6 +101,7 @@ $req = $bdd->prepare('SELECT ventes.id,ventes.timestamp ,moyens_paiement.nom moy
                        FROM ventes ,moyens_paiement 
                        WHERE ventes.id_point_vente = :id_point_vente 
                        AND ventes.id_moyen_paiement = moyens_paiement.id AND DATE(ventes.timestamp) =DATE(CURRENT_TIMESTAMP())
+                       ORDER BY ventes.timestamp DESC
                        LIMIT 0,:offset ');
 
 $req->bindValue('id_point_vente' , $_GET['numero'] , PDO::PARAM_INT );
