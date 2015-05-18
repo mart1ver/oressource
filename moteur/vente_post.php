323ae793555680fc9a1cb5539e0dc16935f3d1ad
Martin VERT <mart1ver@gmail.com>
2015-05-18 16:07:49 +0200

    dater les ventes si besoin

--- moteur/vente_post.php
+++ moteur/vente_post.php
@@ -14,7 +14,60 @@ if(isset($_POST['adh']))
 
 
 
-  
+  if ($_SESSION['saisiec'] == "oui")
+
+
+   {
+    
+
+    // Connexion à la base de données
+    try
+{
+    include('dbconfig.php');
+}
+    catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+// Insertion de la vente (sans les objets vendus) l'aide d'une requête préparée
+  $req = $bdd->prepare('INSERT INTO ventes (adherent, commentaire, id_point_vente, id_moyen_paiement, id_createur) VALUES(?, ?, ?, ?, ?)');
+  $req->execute(array($adh,  $_POST['comm'] , $_POST['id_point_vente'], $_POST['moyen'], $_SESSION['id']));
+  $id_vente = $bdd->lastInsertId();
+    $req->closeCursor();
+//insertion des vendus dans la table vendus
+//$_POST['nlignes'] = le nombre de lignes a ajouter 
+           
+         $i = 1;
+while ($i <= $_POST['nlignes'])
+{
+   //on inserre les valeures pour chaque 'i' ($i = une ligne dans le ticket)   
+   // Insertion du post à l'aide d'une requête préparée
+
+$tid_type_objet = 'tid_type_objet'.$i;
+if(isset($_POST[$tid_type_objet]))
+    {
+$tid_objet ='tid_objet'.$i;
+$tquantite = 'tquantite'.$i;
+$tprix = 'tprix'.$i;
+try
+{
+include('dbconfig.php');
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+$req = $bdd->prepare('INSERT INTO vendus (id_vente,  id_type_dechet, id_objet, quantite, prix, id_createur) VALUES(?,?, ?, ?, ?, ?)');
+$req->execute(array($id_vente ,  $_POST[$tid_type_objet] ,  $_POST[$tid_objet] ,  $_POST[$tquantite], $_POST[$tprix], $_SESSION['id']));
+  $req->closeCursor();
+}
+    $i++;
+}
+// Redirection du visiteur vers la page de gestion des affectation
+  header("Location:../ifaces/ventes.php?numero=".$_POST['id_point_vente']);
+  }else      {
+
+
 // Connexion à la base de données
 		try
 {
@@ -60,6 +113,7 @@ $req->execute(array($id_vente ,  $_POST[$tid_type_objet] ,  $_POST[$tid_objet] ,
 }
 // Redirection du visiteur vers la page de gestion des affectation
 	header("Location:../ifaces/ventes.php?numero=".$_POST['id_point_vente']);
+}
 }
 else { 
 header('Location:../moteur/destroy.php');
