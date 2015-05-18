0dc9d4e6f109209f763ea90aa52f53eee360105f
Martin VERT <mart1ver@gmail.com>
2015-05-18 14:38:52 +0200

    dater les ventes si besoin

--- ifaces/ventes.php
+++ ifaces/ventes.php
@@ -93,8 +93,13 @@ else // SINON (la variable ne contient ni Oui ni Non, on ne peut pas agir)
   </div>
   <div class="panel-body" id="divID">
      <form action="../moteur/vente_post.php" id="formulaire" method="post">
-      date à poser:  <input type="date" name="antidate" style="height:20px;" value=<?php echo date("Y-m-d") ?>>
+
+
+      date de la vente:  <input type="date" name="antidate" style="height:20px;" value=<?php echo date("Y-m-d") ?>>
 <br>
+<br>
+
+
 <ul id="liste" class="list-group">
    <li class="list-group-item">Vente: <?php echo $_GET['numero']?>#<?php echo $numero_vente?>, date: <?php echo date("d-m-Y") ?><br><?php echo $nom_pv;?><br><?php echo $adresse_pv;?>,<br>siret: <?php echo$_SESSION['siret'];?></li>
   
