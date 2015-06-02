0241ab07553c0ee97a01222c136dcc47c51888a1
Martin VERT <mart1ver@gmail.com>
2015-06-02 14:17:54 +0200

    dater les sorties hors boutique si besoin

--- moteur/sortiesr_post.php
+++ moteur/sortiesr_post.php
@@ -15,7 +15,66 @@ $antidate = $_POST['antidate'].date(" H:i:s");
 
 
 
+// Connexion à la base de données
+    try
+{
+    include('dbconfig.php');
+}
+    catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+// Insertion de la collecte (sans les pesées) l'aide d'une requête préparée
+  $req = $bdd->prepare('INSERT INTO sorties (timestamp,id_filiere , classe, id_point_sortie, commentaire, id_createur) VALUES(?,?, ?, ?, ?, ?)');
+  $req->execute(array($antidate,$_POST['id_filiere'], "sortiesr", $_POST['id_point_sortie'], $_POST['commentaire'], $_SESSION['id']));
+  $id_sortie = $bdd->lastInsertId();
+    $req->closeCursor();
 
+
+//insertion des pessés dans la table pesées_collectes
+//on determine '$nombrecat' le nombre de categories maxi (soit l'id maximum)  
+            try
+            {
+            // On se connecte à MySQL
+            include('../moteur/dbconfig.php');
+            }
+            catch(Exception $e)
+            {
+            // En cas d'erreur, on affiche un message et on arrête tout
+            die('Erreur : '.$e->getMessage());
+            }
+            // On ecrit le nombre de categories maxi dans la varialble $nombrecat 
+            $reponse = $bdd->query('SELECT MAX(id) AS nombrecat FROM type_dechets_evac');
+            // On affiche chaque entree une à une
+            while ($donnees = $reponse->fetch())
+            {      
+            $nombrecat = $donnees['nombrecat'];
+             }
+            $reponse->closeCursor(); // Termine le traitement de la requête
+         $i = 1;
+while ($i <= $nombrecat)
+{
+   //on inserre les valeures pour chaque 'i' ($i = id_type dechet) si elles sonts superieures à 0  
+if ($_POST["m".$i] > 0) 
+{
+try
+{
+include('dbconfig.php');
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+// Insertion du post à l'aide d'une requête préparée
+$req = $bdd->prepare('INSERT INTO pesees_sorties (timestamp,masse,  id_sortie, id_type_dechet_evac, id_createur) VALUES(?,?, ?, ?, ?)');
+$req->execute(array($antidate,$_POST["m".$i],  $id_sortie , $i, $_SESSION['id']));
+  $req->closeCursor();
+}
+    $i++;
+}
+// Redirection du visiteur vers la page de gestion des affectation
+
+  header("Location:../ifaces/sortiesr.php?numero=".$_POST['id_point_sortie']);
 
 
 
