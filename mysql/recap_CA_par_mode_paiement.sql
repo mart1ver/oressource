0750365f369035299a0dce0d4a575f717b809f45
Martin VERT <mart1ver@gmail.com>
2015-06-15 13:40:17 +0200

    recap par mod e de paiement

--- mysql/recap_CA_par_mode_paiement.sql
+++ mysql/recap_CA_par_mode_paiement.sql
@@ -1,12 +1,3 @@
----
--- name : bilan_z.sql
--- desc : récapitulatif des encaissements classés par moyen de paiement
---
--- params :
---  :du = la date de debut
---  :au = la date de fin
---  :numero = id du point de vente
-
 SELECT 
 	ventes.id_moyen_paiement AS id_moyen, 
 	moyens_paiement.nom AS moyen,
