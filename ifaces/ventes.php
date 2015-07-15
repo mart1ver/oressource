cbd596e345ba61afa4077f1aa2d00acdff214450
Martin VERT <mart1ver@gmail.com>
2015-07-15 14:00:03 +0200

    rendu monaie à la caisse*

--- ifaces/ventes.php
+++ ifaces/ventes.php
@@ -171,21 +171,21 @@ $('input[name="my-checkbox"]').on('switchChange.bootstrapSwitch', function(event
   <li class="list-group-item">
 
 Somme due:
-  <input type="text" class="form-control " placeholder="€" name="rendua" id="rendua" disabled>
+  <input type="text" class="form-control " placeholder="€" name="rendua" id="rendua" height="20" disabled>
 
 
   </li>
   <li class="list-group-item list-group-item-success">
 
 Réglement
-  <input type="text" class="form-control" placeholder="€" name="rendub" id="rendub" onfocus="fokus(this)" oninput="rendu()">
+  <input type="text" class="form-control" placeholder="€" name="rendub" id="rendub" height="20" onfocus="fokus(this)" oninput="rendu()">
 
 
   </li>
   <li class="list-group-item list-group-item-danger">
     
 <b>A rendre</b>
-  <input type="text" class="form-control" placeholder="€"  name="renduc" id="renduc" disabled>
+  <input type="text" class="form-control" placeholder="€"  name="renduc" id="renduc" height="20" disabled>
 
 
   </li>
