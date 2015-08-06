067ec9978ce42d5f2b522d1f74942a1769a46388
Martin VERT <mart1ver@gmail.com>
2015-08-06 19:03:42 +0200

    bilans sorties hors boutique ....

--- ifaces/bilanhb.php
+++ ifaces/bilanhb.php
@@ -366,6 +366,9 @@ $reponse->closeCursor(); // Termine le traitement de la requête
   </div>
   <div class="panel-body">
     
+
+
+  <?php if ($_GET['numero'] == 0) {  ?>
 <table class="table table-condensed table-striped table table-bordered table-hover" style="border-collapse:collapse;">
     <thead>
       <th style="width:300px">Dons simples</th>
@@ -633,6 +636,291 @@ GROUP BY nom2');
  <?php
              }
 $reponse->closeCursor(); // Termine le traitement de la requête
+
+
+
+}else{
+
+?>
+
+
+<table class="table table-condensed table-striped table table-bordered table-hover" style="border-collapse:collapse;">
+    <thead>
+      <th style="width:300px">Dons simples</th>
+        <tr>
+            <th  style="width:300px">typo</th>
+            <th>masse</th>
+            <th>%</th>
+
+                        
+        </tr>
+    </thead>
+    <tbody>
+       
+
+<?php
+ $reponse = $bdd->prepare('SELECT type_dechets.nom, sum(pesees_sorties.masse) somme
+FROM type_dechets, pesees_sorties, sorties
+WHERE
+type_dechets.id=pesees_sorties.id_type_dechet
+AND
+pesees_sorties.id_sortie = sorties.id
+AND sorties.classe = "sorties"
+AND pesees_sorties.timestamp BETWEEN :du AND :au
+GROUP BY nom
+UNION
+SELECT type_dechets_evac.nom nom2, sum(pesees_sorties.masse) somme
+FROM type_dechets_evac, pesees_sorties, sorties
+WHERE
+type_dechets_evac.id=pesees_sorties.id_type_dechet_evac
+AND
+pesees_sorties.id_sortie = sorties.id
+AND sorties.classe = "sorties"
+AND pesees_sorties.timestamp BETWEEN :du AND :au
+GROUP BY nom2');
+ $reponse->execute(array('du' => $time_debut,'au' => $time_fin ));
+           // On affiche chaque entree une à une
+           while ($donnees = $reponse->fetch())
+           {
+            ?>
+                     
+
+        <tr>
+            <td><?php echo $donnees['nom'] ?></td>
+            <td><?php echo $donnees['somme'] ?></td>
+            <td><?php echo  round($donnees['somme']*100/$mtotcolo, 2)   ; ?></td>      
+        </tr>
+ <?php
+             }
+$reponse->closeCursor(); // Termine le traitement de la requête
+?>
+
+
+
+
+
+   </tbody>
+</table>
+
+
+
+
+
+<table class="table table-condensed table-striped table table-bordered table-hover" style="border-collapse:collapse;">
+    <thead>
+      <th  style="width:300px">Dons aux partenaires</th>
+        <tr>
+            <th  style="width:300px">typo</th>
+            <th>masse</th>
+            <th>%</th>
+
+                        
+        </tr>
+    </thead>
+    <tbody>
+       
+<?php
+ $reponse = $bdd->prepare('SELECT type_dechets.nom, sum(pesees_sorties.masse) somme
+FROM type_dechets, pesees_sorties, sorties
+WHERE
+type_dechets.id=pesees_sorties.id_type_dechet
+AND
+pesees_sorties.id_sortie = sorties.id
+AND sorties.classe = "sortiesc"
+AND pesees_sorties.timestamp BETWEEN :du AND :au
+GROUP BY nom
+UNION
+SELECT type_dechets_evac.nom nom2, sum(pesees_sorties.masse) somme
+FROM type_dechets_evac, pesees_sorties, sorties
+WHERE
+type_dechets_evac.id=pesees_sorties.id_type_dechet_evac
+AND
+pesees_sorties.id_sortie = sorties.id
+AND sorties.classe = "sortiesc"
+AND pesees_sorties.timestamp BETWEEN :du AND :au
+GROUP BY nom2');
+ $reponse->execute(array('du' => $time_debut,'au' => $time_fin ));
+           // On affiche chaque entree une à une
+           while ($donnees = $reponse->fetch())
+           {
+            ?>
+                     
+
+
+            <td><?php echo $donnees['nom'] ?></td>
+            <td><?php echo $donnees['somme'] ?></td>
+            <td><?php echo  round($donnees['somme']*100/$mtotcolo, 2)   ; ?></td>      
+        </tr>
+ <?php
+             }
+$reponse->closeCursor(); // Termine le traitement de la requête
+?>
+
+   </tbody>
+</table>
+
+
+
+
+<table class="table table-condensed table-striped table table-bordered table-hover" style="border-collapse:collapse;">
+    <thead>
+      <th  style="width:300px">dechetterie</th>
+        <tr>
+            <th  style="width:300px">typo</th>
+            <th>masse</th>
+            <th>%</th>
+
+                        
+        </tr>
+    </thead>
+    <tbody>
+       
+<?php
+ $reponse = $bdd->prepare('SELECT type_dechets.nom, sum(pesees_sorties.masse) somme
+FROM type_dechets, pesees_sorties, sorties
+WHERE
+type_dechets.id=pesees_sorties.id_type_dechet
+AND
+pesees_sorties.id_sortie = sorties.id
+AND sorties.classe = "sortiesd"
+AND pesees_sorties.timestamp BETWEEN :du AND :au
+GROUP BY nom
+UNION
+SELECT type_dechets_evac.nom nom2, sum(pesees_sorties.masse) somme
+FROM type_dechets_evac, pesees_sorties, sorties
+WHERE
+type_dechets_evac.id=pesees_sorties.id_type_dechet_evac
+AND
+pesees_sorties.id_sortie = sorties.id
+AND sorties.classe = "sortiesd"
+AND pesees_sorties.timestamp BETWEEN :du AND :au
+GROUP BY nom2');
+ $reponse->execute(array('du' => $time_debut,'au' => $time_fin ));
+           // On affiche chaque entree une à une
+           while ($donnees = $reponse->fetch())
+           {
+            ?>
+                     
+
+
+            <td><?php echo $donnees['nom'] ?></td>
+            <td><?php echo $donnees['somme'] ?></td>
+            <td><?php echo  round($donnees['somme']*100/$mtotcolo, 2)   ; ?></td>      
+        </tr>
+ <?php
+             }
+$reponse->closeCursor(); // Termine le traitement de la requête
+?>
+
+   </tbody>
+</table>
+
+
+
+<table class="table table-condensed table-striped table table-bordered table-hover" style="border-collapse:collapse;">
+    <thead>
+      <th  style="width:300px">poubelles</th>
+        <tr>
+            <th  style="width:300px">type de bac</th>
+            <th>masse</th>
+            <th>%</th>
+
+                        
+        </tr>
+    </thead>
+    <tbody>
+       
+<?php
+ $reponse = $bdd->prepare('SELECT types_poubelles.nom, sum(pesees_sorties.masse) somme
+FROM types_poubelles, pesees_sorties, sorties
+WHERE
+pesees_sorties.id_sortie = sorties.id
+AND
+types_poubelles.id = pesees_sorties.id_type_poubelle
+AND sorties.classe = "sortiesp"
+AND pesees_sorties.timestamp BETWEEN :du AND :au
+GROUP BY nom
+');
+ $reponse->execute(array('du' => $time_debut,'au' => $time_fin ));
+           // On affiche chaque entree une à une
+           while ($donnees = $reponse->fetch())
+           {
+            ?>
+                     
+
+
+            <td><?php echo $donnees['nom'] ?></td>
+            <td><?php echo $donnees['somme'] ?></td>
+            <td><?php echo  round($donnees['somme']*100/$mtotcolo, 2)   ; ?></td>      
+        </tr>
+ <?php
+             }
+$reponse->closeCursor(); // Termine le traitement de la requête
+?>
+
+   </tbody>
+</table>
+
+
+
+<table class="table table-condensed table-striped table table-bordered table-hover" style="border-collapse:collapse;">
+    <thead>
+      <th  style="width:300px">recycleurs</th>
+        <tr>
+            <th  style="width:300px">typo</th>
+            <th>masse</th>
+            <th>%</th>
+
+                        
+        </tr>
+    </thead>
+    <tbody>
+       
+<?php
+ $reponse = $bdd->prepare('SELECT type_dechets.nom, sum(pesees_sorties.masse) somme
+FROM type_dechets, pesees_sorties, sorties
+WHERE
+type_dechets.id=pesees_sorties.id_type_dechet
+AND
+pesees_sorties.id_sortie = sorties.id
+AND sorties.classe = "sortiesr"
+AND pesees_sorties.timestamp BETWEEN :du AND :au
+GROUP BY nom
+UNION
+SELECT type_dechets_evac.nom nom2, sum(pesees_sorties.masse) somme
+FROM type_dechets_evac, pesees_sorties, sorties
+WHERE
+type_dechets_evac.id=pesees_sorties.id_type_dechet_evac
+AND
+pesees_sorties.id_sortie = sorties.id
+AND sorties.classe = "sortiesr"
+AND pesees_sorties.timestamp BETWEEN :du AND :au
+GROUP BY nom2');
+ $reponse->execute(array('du' => $time_debut,'au' => $time_fin ));
+           // On affiche chaque entree une à une
+           while ($donnees = $reponse->fetch())
+           {
+            ?>
+                     
+
+
+            <td><?php echo $donnees['nom'] ?></td>
+            <td><?php echo $donnees['somme'] ?></td>
+            <td><?php echo  round($donnees['somme']*100/$mtotcolo, 2)   ; ?></td>      
+        </tr>
+ <?php
+             }
+$reponse->closeCursor(); // Termine le traitement de la requête
+
+
+
+
+
+}
+
+
+
+
 ?>
 
    </tbody>
