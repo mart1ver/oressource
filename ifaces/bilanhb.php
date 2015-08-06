bbf6eab0bc6ab188d1815b053cad92e42959d553
Martin VERT <mart1ver@gmail.com>
2015-08-06 17:07:49 +0200

    bilans sorties hors boutique ....

--- ifaces/bilanhb.php
+++ ifaces/bilanhb.php
@@ -498,6 +498,43 @@ $reponse->closeCursor(); // Termine le traitement de la requête
     </thead>
     <tbody>
        
+<?php
+ $reponse = $bdd->prepare('SELECT type_dechets.nom, sum(pesees_sorties.masse) somme
+FROM type_dechets, pesees_sorties, sorties
+WHERE
+type_dechets.id=pesees_sorties.id_type_dechet
+AND
+pesees_sorties.id_sortie = sorties.id
+AND sorties.classe = "sorties"
+AND pesees_sorties.timestamp BETWEEN :du AND :au
+GROUP BY nom
+UNION
+SELECT type_dechets_evac.nom nom2, sum(pesees_sorties.masse) somme
+FROM type_dechets_evac, pesees_sorties, sorties
+WHERE
+type_dechets_evac.id=pesees_sorties.id_type_dechet_evac
+AND
+pesees_sorties.id_sortie = sorties.id
+AND sorties.classe = "sorties"
+AND pesees_sorties.timestamp BETWEEN :du AND :au
+GROUP BY nom2');
+ $reponse->execute(array('du' => $time_debut,'au' => $time_fin ));
+           // On affiche chaque entree une à une
+           while ($donnees = $reponse->fetch())
+           {
+            ?>
+                     
+
+
+            <td><?php echo $donnees['nom'] ?></td>
+            <td><?php echo $donnees['somme'] ?></td>
+            <td><?php echo  round($donnees['somme']*100/$mtotcolo, 2)   ; ?></td>      
+        </tr>
+ <?php
+             }
+$reponse->closeCursor(); // Termine le traitement de la requête
+?>
+
    </tbody>
 </table>
 
