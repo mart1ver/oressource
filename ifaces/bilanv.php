17fdd7c39f9675531deef270101c79712f4e0045
Martin VERT <mart1ver@gmail.com>
2015-08-28 13:47:59 +0200

    un peut de mise en page dans le bilan des ventes

--- ifaces/bilanv.php
+++ ifaces/bilanv.php
@@ -286,7 +286,7 @@ print "</table>";
 <div class="col-md-5 col-md-offset-1">
   
 
-<tr><td><h2>chiffre de caisse : </h2></td><?php echo  "<td> <h2>".$mtotcolo- $mtotcolo2." €</h2> </td></tr>";?> 
+<tr><td><h2>chiffre de caisse : </td><?php echo  "<td> ".$mtotcolo- $mtotcolo2." €</h2> </td></tr>";?> 
 
 <table class="table table-hover">
       <thead>
