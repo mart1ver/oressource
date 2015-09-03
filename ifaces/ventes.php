656e5a9b5aa788078802f963c6b88507b196b2a0
Martin VERT <mart1ver@gmail.com>
2015-09-03 13:58:01 +0200

    legere amelioration du redu monnaie

--- ifaces/ventes.php
+++ ifaces/ventes.php
@@ -166,21 +166,21 @@ $('input[name="my-checkbox"]').on('switchChange.bootstrapSwitch', function(event
   <li class="list-group-item">
 
 Somme due:
-  <input type="text" class="form-control " placeholder="€" name="rendua" id="rendua" style=" height:22px;" disabled>
+  <input type="text" class="form-control " maxlength="6" placeholder="€" name="rendua" id="rendua" style=" height:22px;" disabled>
 
 
   </li>
   <li class="list-group-item list-group-item-success">
 
 Réglement
-  <input type="text" class="form-control" placeholder="€" name="rendub" id="rendub" style=" height:25px;" onfocus="fokus(this)" oninput="rendu()">
+  <input type="text" class="form-control" maxlength="6" placeholder="€" name="rendub" id="rendub" style=" height:25px;" onfocus="fokus(this)" oninput="rendu()">
 
 
   </li>
   <li class="list-group-item list-group-item-danger">
     
 <b>A rendre</b>
-  <input type="text" class="form-control" placeholder="€"  name="renduc" id="renduc" style=" height:25px;" disabled>
+  <input type="text" class="form-control" maxlength="6" placeholder="€"  name="renduc" id="renduc" style=" height:25px;" disabled>
 
 
   </li>
