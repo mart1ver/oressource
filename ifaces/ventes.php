09181f1b7c403a986f063bf2b7286466c8dbed83
Martin VERT <mart1ver@gmail.com>
2015-09-03 14:12:38 +0200

    legere amelioration du redu monnaie

--- ifaces/ventes.php
+++ ifaces/ventes.php
@@ -384,7 +384,7 @@ $(document).ready(function(){
 function rendu() 
 
 {
-  if(rendub > 0){
+  if(document.getElementById('rendub').value > 0){
 
 
 document.getElementById('renduc').value = document.getElementById('rendub').value -  document.getElementById('rendua').value;
