0da92ea03862006c5599cee1c90b5e0429010981
martin vert <mart1ver@gmail.com>
2015-09-19 18:49:21 +0200

    modifications sur remboursements caisse

--- moteur/verif_remb_post.php
+++ moteur/verif_remb_post.php
@@ -10,13 +10,22 @@
 
 
     <?php session_start();
-   require_once('../moteur/dbconfig.php');
+  
 
 //Vérification des autorisations de l'utilisateur et des variables de session requises pour l'utilisation de cette requête:
  if (isset($_SESSION['id']) AND $_SESSION['systeme'] = "oressource" AND (strpos($_SESSION['niveau'], 'v'.$_GET['numero']) !== false))
 {
 
-
+  try
+            {
+            // On se connecte à MySQL
+            include('../moteur/dbconfig.php');
+            }
+            catch(Exception $e)
+            {
+            // En cas d'erreur, on affiche un message et on arrête tout
+            die('Erreur : '.$e->getMessage());
+            }
   // On recupère tout le contenu de la table point de collecte
             $reponse = $bdd->query('SELECT cr FROM `description_structure`');
  
