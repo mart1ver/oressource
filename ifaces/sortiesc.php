96a829a41c737fa40be8a08343160d740be81e97
martin vert <mart1ver@gmail.com>
2015-09-19 17:00:42 +0200

    affichage des onglets de sorties hb QUE si autorisé

--- ifaces/sortiesc.php
+++ ifaces/sortiesc.php
@@ -178,7 +178,7 @@ function tdechet_clear()
 
  <ul class="nav nav-tabs">
   <?php if ($_SESSION['affsp'] == "oui"){ ?><li><a href="<?php echo  "sortiesp.php?numero=" . $_GET['numero']?>">Poubelles</a></li><?php } ?>
-  <?php if ($_SESSION['affss'] == "oui"){ ?><li class="active"><a>Sorties partenaires</a></li><?php } else { } ?>
+  <?php if ($_SESSION['affss'] == "oui"){ ?><li class="active"><a>Sorties partenaires</a></li><?php } else {header('Location:../moteur/destroy.php');} ?>
   <?php if ($_SESSION['affsr'] == "oui"){ ?><li><a href="<?php echo  "sortiesr.php?numero=" . $_GET['numero']?>">Recyclage</a></li><?php } ?>
   <?php if ($_SESSION['affsd'] == "oui"){ ?> <li><a href="<?php echo  "sorties.php?numero=" . $_GET['numero']?>">Don</a></li><?php } ?>
   <?php if ($_SESSION['affsde'] ="sortiesr.php?numero=" . $_GET['numero']= "oui"){ ?><li><a href="<?php echo  "sortiesd.php?numero=" . $_GET['numero']?>">Déchetterie</a></li><?php } ?>
