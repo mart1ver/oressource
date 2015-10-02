c5cdce77695114d0352175f0441141fc4feaa1cd
Axel Viala <axel.viala@darnuria.eu>
2015-10-02 16:32:20 +0200

    Oressource fonctionne enfin sous Firefox.
    Modification effectué avec la ligne de commande suivante:
    `sed -i -e 's/innerText/textContent/g' ifaces/*.php`
    L'impression des tickets semble encore fonctionner.

--- ifaces/sortiesc.php
+++ ifaces/sortiesc.php
@@ -101,8 +101,8 @@ function tdechet_write(y,z)
  {
           if (document.getElementById("number").value > 0 && document.getElementById("number").value < <?php echo $pesee_max;?>) 
           {
-            document.getElementById("massetot").innerText = parseFloat(document.getElementById("massetot").innerText) + parseFloat(document.getElementById("number").value) ;
-             document.getElementById(y).innerText = parseFloat(document.getElementById(y).innerText) + parseFloat(document.getElementById("number").value)  ;
+            document.getElementById("massetot").textContent = parseFloat(document.getElementById("massetot").textContent) + parseFloat(document.getElementById("number").value) ;
+             document.getElementById(y).textContent = parseFloat(document.getElementById(y).textContent) + parseFloat(document.getElementById("number").value)  ;
               document.getElementById(z).value = parseFloat(document.getElementById(z).value) + parseFloat(document.getElementById("number").value)  ;
              document.getElementById("number").value = "";  
              document.getElementById("najout").value = parseInt(document.getElementById("najout").value)+1;
@@ -119,7 +119,7 @@ function tdechet_clear()
            {
 
            ?>
-    document.getElementById('<?php echo$donnees['nom']?>').innerText = "0"  ;
+    document.getElementById('<?php echo$donnees['nom']?>').textContent = "0"  ;
     document.getElementById('<?php echo$donnees['id']?>').value = "0" ; 
 <?php }
 
@@ -135,7 +135,7 @@ function tdechet_clear()
            {
 
            ?>
-    document.getElementById('<?php echo "d".$donnees['nom']?>').innerText = "0"  ;
+    document.getElementById('<?php echo "d".$donnees['nom']?>').textContent = "0"  ;
     document.getElementById('<?php echo "d".$donnees['id']?>').value = "0" ; 
 <?php }
 
--- ifaces/sortiesp.php
+++ ifaces/sortiesp.php
@@ -87,9 +87,9 @@ function tdechet_write(y,z)
 
 if (document.getElementById("number").value-parseFloat(document.getElementById("m"+y).value)  > 0 && document.getElementById("number").value < <?php echo $pesee_max;?>) 
 {
-  document.getElementById("massetot").innerText = parseFloat(parseFloat(document.getElementById("massetot").innerText) + (parseFloat(document.getElementById("number").value) - parseFloat(document.getElementById("m"+y).value))).toFixed(2);
+  document.getElementById("massetot").textContent = parseFloat(parseFloat(document.getElementById("massetot").textContent) + (parseFloat(document.getElementById("number").value) - parseFloat(document.getElementById("m"+y).value))).toFixed(2);
   document.getElementById("najout").value = parseInt(document.getElementById("najout").value)+1;
-    document.getElementById(y).innerText = ((parseFloat(document.getElementById(y).innerText) + parseFloat(document.getElementById("number").value))-parseFloat(document.getElementById("m"+y).value)).toFixed(2)  ;
+    document.getElementById(y).textContent = ((parseFloat(document.getElementById(y).textContent) + parseFloat(document.getElementById("number").value))-parseFloat(document.getElementById("m"+y).value)).toFixed(2)  ;
      document.getElementById(z).value = (parseFloat(document.getElementById(z).value) + parseFloat(document.getElementById("number").value)-parseFloat(document.getElementById("m"+y).value)).toFixed(2)  ;
     document.getElementById("number").value = "";  
 }
@@ -108,7 +108,7 @@ function tdechet_clear()
            {
 
            ?>
-    document.getElementById('<?php echo$donnees['nom']?>').innerText = "0"  ;
+    document.getElementById('<?php echo$donnees['nom']?>').textContent = "0"  ;
     document.getElementById(<?php echo$donnees['id']?>).value = "0" ;
 <?php }
 
