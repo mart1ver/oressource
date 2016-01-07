eaa53a25ede663fd37fc8095d4908b6bd146cb11
martin vert <mart1ver@gmail.com>
2016-01-07 16:05:10 +0100

    essais data tables

---
+++ ifaces/tables.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+  </head>
+<body>
+<table id="table_id" class="display">
+    <thead>
+        <tr>
+            <th>Column 1</th>
+            <th>Column 2</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Row 1 Data 1</td>
+            <td>Row 1 Data 2</td>
+        </tr>
+        <tr>
+            <td>Row 2 Data 1</td>
+            <td>Row 2 Data 2</td>
+        </tr>
+    </tbody>
+</table>
+
+</body>
+</html>
