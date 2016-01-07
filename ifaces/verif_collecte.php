9883f422791fda652ada803971d2865da54932cb
martin vert <mart1ver@gmail.com>
2016-01-07 17:38:52 +0100

    essais data tables, traduction fr

--- ifaces/verif_collecte.php
+++ ifaces/verif_collecte.php
@@ -75,6 +75,7 @@ function $_GET(param) {
   return vars;
 }
 
+
                $(document).ready(function() {
 
                   var cb = function(start, end, label) {
@@ -83,7 +84,7 @@ function $_GET(param) {
                     //alert("Callback has fired: [" + start.format('MMMM D, YYYY') + " to " + end.format('MMMM D, YYYY') + ", label = " + label + "]");
                   }
 
-var dateuno = $_GET(date1)
+var dateuno = $_GET(date1);
 console.log( moment());
 console.log (dateuno);
                   var optionSet1 = {
