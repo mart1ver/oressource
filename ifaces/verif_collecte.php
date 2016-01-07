4cc250ad5e162075ab21ccf20a0f936f67680a9c
martin vert <mart1ver@gmail.com>
2016-01-07 17:44:23 +0100

    essais data tables, traduction fr

--- ifaces/verif_collecte.php
+++ ifaces/verif_collecte.php
@@ -88,7 +88,7 @@ var dateuno = $_GET('date1');
 console.log( moment());
 console.log (dateuno);
                   var optionSet1 = {
-                    startDate: dateuno,
+                    startDate: '01/01/2010',
                     endDate: moment(),
                     minDate: '01/01/2010',
                     maxDate: '12/31/2020',
