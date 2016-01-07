38274a3de40e42a83cc29a71bcd6037002333aa3
martin vert <mart1ver@gmail.com>
2016-01-07 20:12:37 +0100

    jours.php

--- ifaces/jours.php
+++ ifaces/jours.php
@@ -184,17 +184,7 @@ new Morris.Area({
   // Chart data records -- each entry in this array corresponds to a point on
   // the chart.
   data: [
-   { y: '2006', a: 100, b: 90 },
-    { y: '2007', a: 75,  b: 65 },
-    { y: '2008', a: 50,  b: 40 },
-    { y: '2009', a: 75,  b: 65 },
-    { y: '2010', a: 50,  b: 40 },
-    { y: '2011', a: 75,  b: 65 },
-    { y: '2012', a: 100, b: 90 }
-
-
-
-
+  
 
 <?php 
             // On recupère tout le contenu de la table affectations
