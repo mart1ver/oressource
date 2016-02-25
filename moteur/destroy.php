98cd9219cc25d0141d2e53dc83c5561b555ffcb5
mart1ver <mart1ver@gmail.com>
2016-02-25 16:50:04 +0100

    timeout sur les session reglable

--- moteur/destroy.php
+++ moteur/destroy.php
@@ -1,12 +1,12 @@
 <?php
 		session_start();
-	// Suppression des cookies de connexion automatique
+// Suppression des variables de session et de la session
+	$_SESSION = array();
+	 session_unset();
+     session_destroy();
+
+// Suppression des cookies de connexion automatique
 		setcookie('login', '');
 		setcookie('pass', '');
-    // Suppression des variables de session et de la session
-	    $_SESSION = array();
-	    session_unset();
-        session_destroy();
-    //redirection vers l'index    
 		header ('location:../index.php');
 ?>
