a71b4a0723e52b21ef8ed35a379a12c43744fc02
mart1ver <mart1ver@gmail.com>
2016-04-12 12:58:09 +0200

    masse a la vente premier jet

--- js/ventes.js
+++ js/ventes.js
@@ -145,7 +145,7 @@ function ajout() {
       fait = "oui";}
          }else
          {
-          if(fait == "non"){
+          
           document.getElementById('liste').innerHTML += '<li class="list-group-item" name="ligne'+parseInt(document.getElementById('nlignes').value)+'" id="ligne'+parseInt(document.getElementById('nlignes').value)+'"><span class="badge">'+parseFloat(parseFloat(document.getElementById('prix').value)*parseFloat(document.getElementById('quantite').value)).toFixed(2)+'€'+'</span><span class="glyphicon glyphicon-remove" aria-hidden="true"    onclick="javascirpt:suprime('+"'ligne"+parseInt(document.getElementById('nlignes').value)+"');"+'"></span>&nbsp;&nbsp;'+document.getElementById('quantite').value+' * '+document.getElementById('nom_objet0').value
         +'<input type="hidden"  id="tid_type_objet'+parseInt(document.getElementById('nlignes').value)+'" name="tid_type_objet'+parseInt(document.getElementById('nlignes').value)+'"value="'+document.getElementById('id_type_objet').value+'">'
         +'<input type="hidden"  id="tid_objet'+parseInt(document.getElementById('nlignes').value)+'" name="tid_objet'+parseInt(document.getElementById('nlignes').value)+'"value="'+document.getElementById('id_objet').value+'">'
@@ -159,8 +159,7 @@ function ajout() {
       document.getElementById('id_type_objet').value = "";
       document.getElementById('id_objet').value = "";
       document.getElementById('nom_objet0').value = "";
-      fait = "oui";
-         }}
+      }
     }
   } else {
     var prixtemp = document.getElementById('prix').value;
@@ -235,7 +234,7 @@ function ajout() {
       fait = "oui"}
          }else
          {
-          if(fait == "non"){
+          
           document.getElementById('liste').innerHTML += '<li class="list-group-item" name="ligne'+parseInt(document.getElementById('nlignes').value)+'" id="ligne'+parseInt(document.getElementById('nlignes').value)+'"><span class="badge">'+parseFloat(document.getElementById('prix').value).toFixed(2)+'€'+'</span><span class="glyphicon glyphicon-remove" aria-hidden="true"    onclick="javascirpt:suprime('+"'ligne"+parseInt(document.getElementById('nlignes').value)+"');"+'"></span>&nbsp;&nbsp;'+document.getElementById('quantite').value+' * '+document.getElementById('nom_objet0').value
         +'<input type="hidden"  id="tid_type_objet'+parseInt(document.getElementById('nlignes').value)+'" name="tid_type_objet'+parseInt(document.getElementById('nlignes').value)+'"value="'+document.getElementById('id_type_objet').value+'">'
         +'<input type="hidden"  id="tid_objet'+parseInt(document.getElementById('nlignes').value)+'" name="tid_objet'+parseInt(document.getElementById('nlignes').value)+'"value="'+document.getElementById('id_objet').value+'">'
@@ -249,7 +248,7 @@ function ajout() {
       document.getElementById('id_type_objet').value = "";
       document.getElementById('id_objet').value = "";
       document.getElementById('nom_objet0').value = "";
-         }}
+         }
     }
   }
 
@@ -258,6 +257,14 @@ function ajout() {
   }
   document.getElementById('rendua').value = document.getElementById('ptot').value ;
 }
+
+
+
+
+
+
+
+
 
 function edite(nom,prix,id_type_objet,id_objet) {
   document.getElementById('nom_objet').innerHTML = "<label>"+nom+"</label>";
