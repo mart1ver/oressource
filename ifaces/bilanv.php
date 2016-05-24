b64ffc8443d151a77980bb682b6584919190ea3c
mart1ver <mart1ver@gmail.com>
2016-05-24 14:23:47 +0200

    masse dans les bilans ventes

--- ifaces/bilanv.php
+++ ifaces/bilanv.php
@@ -365,7 +365,7 @@ Récapitulatif par type d'objet
           <th>chiffre dégagé</th>
           <th>quantité vendue</th>
           <th>somme remboursée</th>
-          <th>quantité remboursée</th>
+          <th>q. remboursée</th>
           <th>masse pésee</th>
           <th>masse totale estimée</th>
           
@@ -522,6 +522,7 @@ echo round((($Mm*$Nt)-($Mm*$Mp))+$Mtpe, 2)." Kgs.";
       <td></td>
     </tr>
   </tfoot>
+  
     </table>
 <h3>
 Récapitulatif des masses pesées à la caisse 
