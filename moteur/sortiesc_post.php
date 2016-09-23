3720a77a16f1fc69d635b837023ea36055e89469
martin vert <mart1ver@gmail.com>
2016-09-23 09:53:47 +0200

    lien vers  jours.php dans le bilan

--- moteur/sortiesc_post.php
+++ moteur/sortiesc_post.php
@@ -229,7 +229,6 @@ $req->execute(array($_POST["d".$i],  $id_sortie , $i, $_POST['id_user']));
 // Redirection du visiteur vers la page de gestion des affectation
 	header("Location:../ifaces/sortiesc.php?numero=".$_POST['id_point_sortie']);
 
-}
 
 }
 //Vérification des autorisations de l'utilisateur et des variables de session requises pour l'utilisation de cette requête:
