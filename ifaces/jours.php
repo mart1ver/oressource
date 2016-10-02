6770b2f0df25f9b2be6c1722c51beb3ca3795cf4
Martin Vert <mart1ver@gmail.com>
2016-10-02 19:03:03 +0200

    test

--- ifaces/jours.php
+++ ifaces/jours.php
@@ -269,7 +269,8 @@ echo "<h3>Évolution des masses totales évacuées hors boutique en ". $type ."<
 
              ?> Kgs.
 <div id="sorties" style="height: 180px;"></div>
-<?php } ?>
+<?php }
+echo $masse_moy_jour."efsdf"; ?>
 
 <?php 
  $interm = 0;
