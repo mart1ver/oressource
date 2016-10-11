e7ca7dd176c30a33af93da5868868dd08b696fc5
Martin Vert <mart1ver@gmail.com>
2016-10-11 17:21:51 +0200

    config recycleurs

--- ifaces/edition_filieres_sortie.php
+++ ifaces/edition_filieres_sortie.php
@@ -114,8 +114,9 @@ require_once('../moteur/dbconfig.php');
 $req2 = $bdd->prepare("SELECT nom FROM type_dechets_evac WHERE id = :id ");
 $req2->execute(array('id' => $arrayElement));
 $donnees2 = $req2->fetch();
-echo $donnees2['nom']." ";
-
+if($donnees2['nom'] != ""){
+echo $donnees2['nom'].", ";
+}
               $req2->closeCursor(); // Termine le traitement de la requête
 
                        
