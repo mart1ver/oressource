1f8bb8a5fd4ebef474eccdd700519fc20d3d9e9d
Martin Vert <mart1ver@gmail.com>
2016-10-18 17:42:06 +0200

    transformation de l'interface de collecte de maniere à encaisser plusieurstypes_dechets_evac sur le meme bon de sortie recycleur et pour un meme recycleur...

--- js/utilitaire.js
+++ js/utilitaire.js
@@ -74,7 +74,7 @@ function aff_dechets_recycle() {
   const tabtyps = tabref2[1].split('a');
   for (index = 0, len = tabtyps.length; index < len; ++index) {
     console.log(tabtyps[index]);
-
+document.getElementById(tabtyps[index]).style.display = "block";
     
 }
 
