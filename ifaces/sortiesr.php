73cf22ccc7f9e3fa8db1b2926c7e7b4219a78f1d
martin vert <mart1ver@gmail.com>
2016-10-20 18:26:31 +0200

    bugfix plusieurs types de dechets pour les recycleurs

--- ifaces/sortiesr.php
+++ ifaces/sortiesr.php
@@ -84,7 +84,7 @@ function tdechet_clear()
 
            ?>
     document.getElementById('<?php echo$donnees['id']?>').textContent = "0"  ;
-    document.getElementById('m'+<?php echo$donnees['id']?>).value = "0" ; 
+    document.getElementById('m'<?php echo$donnees['id']?>).value = "0" ; 
     document.getElementById('b'<?php echo$donnees['id']?>).style.display = "none";
 <?php }
 
