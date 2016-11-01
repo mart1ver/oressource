6b2ba3ddf761b6d4bdd214cffcd46f3d81e9ee38
Martin Vert <mart1ver@gmail.com>
2016-11-01 16:05:38 +0100

    trame pour l'interface de pesées pour stats

--- js/ventes.js
+++ js/ventes.js
@@ -483,6 +483,7 @@ function edite_stats(nom,prix,id_type_objet,id_objet) {
   document.getElementById('id_type_objet').value = parseFloat(id_type_objet);
   document.getElementById('id_objet').value = parseFloat(id_objet);
   document.getElementById('nom_objet0').value = nom;
+  document.getElementById("masse").focus();
 }
 
 function encaisse() {
