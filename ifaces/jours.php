95c12dddedc32e6f050f2ddcd370796b99f1160f
Martin Vert <mart1ver@gmail.com>
2017-02-27 19:23:12 +0100

    display sous forme de barres (une barre = 1 jour) les lignes c'est assez trompeur...

--- ifaces/jours.php
+++ ifaces/jours.php
@@ -218,7 +218,7 @@ ORDER BY time');
             ],
             xkey: 'y',
             ykeys: ['a'],
-            labels: ['Masse collectée']
+            labels: ['Masse collectée'],
             resize: true,
                      
            
