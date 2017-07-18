0db4d5f99e2689919ce882d0a617c4a9561ddd92
Yvan Sraka <yvan.sraka@gmail.com>
2017-07-18 21:34:52 +0200

    Ajout d'une configuration ESLint à Travis CI

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "warn",
+            2
+        ],
+        "linebreak-style": [
+            "warn",
+            "unix"
+        ],
+        "quotes": [
+            "warn",
+            "double"
+        ],
+        "semi": [
+            "warn",
+            "always"
+        ]
+    }
+};
