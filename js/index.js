a355b39ee891e5f1af3ff92075baf87cdb437295
Yvan Sraka <yvan.sraka@gmail.com>
2017-07-23 23:20:12 +0200

    Suppression des anciennes dépendances JS

---
+++ js/index.js
@@ -0,0 +1,11 @@
+import 'jquery';
+import 'bootstrap'
+import 'bootstrap-switch';
+import 'daterangepicker';
+// import 'morrisjs'; // TODO: https://github.com/morrisjs/morris.js/issues/697
+import 'moment';
+
+import './numpad.js';
+import './ticket.js';
+import './utils.js';
+import './ventes.js';
