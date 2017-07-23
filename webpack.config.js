d131abb664c8f83c475123b9da4d0ee7226751c7
Yvan Sraka <yvan.sraka@gmail.com>
2017-07-23 23:19:22 +0200

    Creation d'une configuration de base pour Webpack JS

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+
+module.exports = {
+  entry: './js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  }
+};
