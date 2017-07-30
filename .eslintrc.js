e5a296a292771f10f0148a2e85199f255c09f8d3
Yvan Sraka <yvan.sraka@gmail.com>
2017-07-30 16:31:36 +0200

    Changement du paramètre de la configuration ESLint 'no-unused-vars' à 'warn' au lieu de 'error'

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,31 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": [
-            "warn",
-            2
-        ],
-        "linebreak-style": [
-            "warn",
-            "unix"
-        ],
-        "quotes": [
-            "warn",
-            "single"
-        ],
-        "semi": [
-            "warn",
-            "always"
-        ]
-    }
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "rules": {
+    "indent": [
+      "warn", 2
+    ],
+    "linebreak-style": [
+      "warn", "unix"
+    ],
+    "quotes": [
+      "warn", "single"
+    ],
+    "semi": [
+      "warn", "always"
+    ],
+    "no-unused-vars": [
+      "warn", {
+        "vars": "all",
+        "args": "after-used",
+        "ignoreRestSiblings": false
+      }
+    ]
+  }
 };
