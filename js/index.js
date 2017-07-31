df79be5ac19507723edbee6c644b6b5be07c8620
Yvan Sraka <yvan.sraka@gmail.com>
2017-07-31 19:14:07 +0200

    Utilisation de webpack uniquement pour les dépendances externes

--- js/index.js
+++ js/index.js
@@ -4,8 +4,3 @@ import 'bootstrap-switch';
 import 'daterangepicker';
 import 'morris.js';
 import 'moment';
-
-import './numpad.js';
-import './ticket.js';
-import './utils.js';
-import './ventes.js';
