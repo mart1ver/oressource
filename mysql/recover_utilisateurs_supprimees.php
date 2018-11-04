4bf74869b88c042b5db315295935b3c1e26b22f2
Axel Viala <axel.viala@darnuria.eu>
2018-11-04 17:27:02 +0100

    Fix script recupération utilisateurs.

--- mysql/recover_utilisateurs_supprimees.php
+++ mysql/recover_utilisateurs_supprimees.php
@@ -2,39 +2,43 @@
 
 require_once '../moteur/dbconfig.php';
 
-function recover_utilisateurs(int $admin, string $table, string $col): string {
-  return ("update $table
-  set $col = 1
-    where not exists (select 1
-    from utilisateurs u
-    where u.id = $table.$col);\n");
+
+function recover_utilisateurs(int $admin, string $table, array $fields): string {
+  return ("update $table as T
+  set T.$fields[0] = $admin,
+      T.$fields[1] = $admin
+  where not exists (select 1
+  from utilisateurs U
+  where U.id = T.$fields[0]);
+");
 }
 
 function recover(PDO $bdd, string $database_name, int $admin) {
   $tables = $bdd->query("SELECT table_name FROM information_schema.tables
     where table_schema=\"$database_name\"");
-
+  
+  $fields = [ 'id_createur', 'id_last_hero'];
   foreach ($tables as $t) {
     $name = $t['table_name'];
 
-    if ($name === NULL) {
+    if ($name === NULL || $name === 'utilisateurs') {
       continue;
     }
 
-    $sql = (
-      recover_utilisateurs($admin, $name, "id_createur")
-    . recover_utilisateurs($admin, $name, "id_last_hero")
-    . "\n"
-    );
-    $bdd->query();
+    $sql = recover_utilisateurs($admin, $name, $fields);
+    $bdd->query($sql);
   }
 }
 
-
+/*
+ * Ce script récupére les utilisateurs supprimées et les réapparentes à un utilisateur
+ * administrateur.
+ * Modifier la variable $admin pour changer cet utilisateur.
+ */
 function main() {
   global $bdd;
-  //
-  $database_name = "oressource3";
+  global $base;
+  $database_name = $base;
 
   // Note: Cas de la petite rockette
   $admin = 1;
