edf0607decdd9eb6e6d445b40c1aeadfbd0c73f9
Axel Viala <axel.viala@darnuria.eu>
2019-05-10 22:48:39 +0200

    Deletion de code mort et nettoyage.

--- ifaces/modification_localites.php
+++ ifaces/modification_localites.php
@@ -18,49 +18,34 @@
  */
 session_start();
 require_once('../moteur/dbconfig.php');
+require_once('../core/requetes.php');
 
 //Vérification des autorisations de l'utilisateur et des variables de session requisent pour l'affichage de cette page:
 if (isset($_SESSION['id']) && $_SESSION['systeme'] === 'oressource' && (strpos($_SESSION['niveau'], 'k') !== false)) {
   require_once 'tete.php';
+  $id = (int) $_POST['id'];
+
+  $localite = localites_id($bdd, $id);
   ?>
   <div class="container">
     <h1>Gestion des localités</h1>
-    <div class="panel-heading">Modifier les données concernant la localité n° <?= $_POST['id']; ?>, <?= $_POST['nom']; ?>. </div>
-    <?php
-//on obtien la couleur de la localité dans la base
-
-    $req = $bdd->prepare('SELECT couleur FROM localites WHERE id = :id ');
-    $req->execute(['id' => $_POST['id']]);
-    $donnees = $req->fetch();
-
-    $couleur = $donnees['couleur'];
-
-    $req->closeCursor();
-    ?>
+    <div class="panel-heading">Modifier les données concernant la localité n° <?= $id ?>, <?= $localite['nom']; ?>. </div>
 
     <div class="panel-body">
       <div class="row">
         <form action="../moteur/modification_localites_post.php" method="post">
-          <input type="hidden" name ="id" id="id" value="<?= $_POST['id']; ?>">
-
-          <div class="col-md-2"><label for="nom">Nom:</label> <input type="text"value ="<?= $_POST['nom']; ?>" name="nom" id="nom" class="form-control " required autofocus></div>
-          <div class="col-md-3"><label for="addresse">commentaire:</label> <input type="text"value ="<?= $_POST['commentaire']; ?>" name="commentaire" id="commentaire" class="form-control" required></div>
-          <div class="col-md-3"><label for="commentaire">Lien externe:</label> <input type="url" value ="<?= $_POST['lien']; ?>" name="lien" id="lien" class="form-control "  ></div>
-          <div class="col-md-1"><label for="couleur">Couleur:</label> <input type="color"value ="<?= $couleur; ?>"name="couleur" id="couleur" class="form-control" required></div>
+          <input type="hidden" name ="id" id="id" value="<?= $id ?>">
+          <div class="col-md-2"><label for="nom">Nom:</label> <input type="text"value ="<?= $localite['nom'] ?>" name="nom" id="nom" class="form-control " required autofocus></div>
+          <div class="col-md-3"><label for="addresse">commentaire:</label> <input type="text"value ="<?= $localite['commentaire'] ?>" name="commentaire" id="commentaire" class="form-control" required></div>
+          <div class="col-md-3"><label for="commentaire">Lien externe:</label> <input type="url" value ="<?= $localite['lien'] ?>" name="lien" id="lien" class="form-control "  ></div>
+          <div class="col-md-1"><label for="couleur">Couleur:</label> <input type="color"value ="<?= $localite['couleur'] ?>"name="couleur" id="couleur" class="form-control" required></div>
           <div class="col-md-1"><br><button name="creer" class="btn btn-warning">Modifier!</button></div>
         </form>
         <br>
         <a href="edition_localites.php">
-          <button name="creer" class="btn btn">Anuler</button>
+          <button name="creer" class="btn btn">Annuler</button>
         </a>
       </div>
-    </div>
-
-    <br>
-    <div class="row">
-      <div class="col-md-4"></div>
-      <div class="col-md-4"><br> </div>
-      <div class="col-md-4"></div>
     </div>
   </div>
   </div>
