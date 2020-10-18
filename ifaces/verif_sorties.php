273bba10d4571d470a5a29d2a5e624dda2ca2d7a
Axel Viala <axel.viala@darnuria.eu>
2020-10-18 23:53:16 +0200

    La classe ne sert plus dans ce composant

--- ifaces/verif_sorties.php
+++ ifaces/verif_sorties.php
@@ -205,14 +205,12 @@ if (is_valid_session() && is_allowed_verifications()) {
     VerifSortiesTable(array_merge($base, [
       'h3' => 'Dons simples :',
       'data' => $sortiesDon,
-      'classe' => ''
     ]))
     ?>
     <?=
     VerifSortiesTable(array_merge($base, [
       'h3' => 'Sorties conventionées :',
       'data' => $sortiesConventions,
-      'classe' => 'c'
     ]))
     ?>
 
@@ -221,7 +219,6 @@ if (is_valid_session() && is_allowed_verifications()) {
     VerifSortiesTable(array_merge($base, [
       'h3' => 'Sorties recyclage:',
       'data' => $sortiesRecyclage,
-      'classe' => 'r'
     ]));
     ?>
 
